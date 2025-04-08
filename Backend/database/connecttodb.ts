7ba3e2f88b5049bd7b7309605b0a14daa1372a7d
Ramesh(RCG) <ramrshrcg@gmail.com>
2025-04-08 19:10:19 +0545

    mail

--- Backend/database/connecttodb.ts
+++ Backend/database/connecttodb.ts
@@ -1,30 +1,37 @@
+import Product from '../Model/productModel';
 import User from '../Model/userModel';
 import { envConfig } from './../config/envConfig';
 import { Sequelize } from "sequelize-typescript";
 
 
-const sequalize  = new Sequelize (`${envConfig.db}`,{
-    models:[User]
-}) 
+const sequalize = new Sequelize(`${envConfig.db}`, {
+    models: [User,
+        Product,
+        
+        ],//just add comma and add the no if tables
+        
+    
+
+
+})
 
 // const sequalize = new Sequelize(envConfig.db as string,{
 //     models : [__dirname + './Model']
 // })
 try {
-    sequalize.authenticate().then(()=>
-    {
+    sequalize.authenticate().then(() => {
         console.log("connected  to sequalize")
 
 
-    }).catch(err=>{
+    }).catch(err => {
         console.log('not connected ', err)
     })
 
-    
+
 } catch (error) {
     console.log(error);
 }
-sequalize.sync({force : false,alter:false}).then(()=>{
+sequalize.sync({ force: false, alter: true }).then(() => {
     console.log("synced !!")
 })//wo this also run hai ta
 
